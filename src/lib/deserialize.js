228d1a805dfc69d8899b1d12896ab851f5b82b7e
Mihailescu Marian Valentin <mariandev96@gmail.com>
2014-07-23 12:24:02 +0300

    Update deserialize.js

--- src/lib/deserialize.js
+++ src/lib/deserialize.js
@@ -61,7 +61,7 @@
       inferredType = 'date';
     }
     // delegate deserialization via type string
-    return typeHandlers[inferredType](value, currentValue);
+    return inferredType !== 'undefined'?typeHandlers[inferredType](value, currentValue):undefined;
   }
 
   // exports
